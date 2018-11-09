12d973bd8e73d9ec3f49aae38f7e5ab26c6eec2a
MatskoA <AMatsko54@mail.ru>
2018-11-09 09:41:57 +0700

    Add input

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 # TODO: входные данные
+S1 = 0.1
+S2 = 0.15
+S3 = 0.25
+S4 = 0.28
+S5 = 0.33
+S6 = 0.35
+S7 = 0.396
 
+personalD1 = 9075
+personalD2 = 36900
+personalD3 = 89350
+personalD4 = 186350
+personalD5 = 405100
+personalD6 = 406750
+
+familyD1 = 2 * personalD1
+familyD2 = 2 * personalD2
+familyD3 = 148850
+familyD4 = 226850
+familyD5 = personalD5
+familyD6 = 457600
+
+singleparentD1 = 12950
+singleparentD2 = 49400
+singleparentD3 = 127550
+singleparentD4 = 206600
+singleparentD5 = personalD5
+singleparentD6 = 432200
 
 # TODO: формулы
 
