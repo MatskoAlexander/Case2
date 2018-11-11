ee23744c418a3cd3895ca36c5c622ba9ce4d14dd
AliceDarkmoon55 <alexandra.shunina@gmail.com>
2018-11-11 20:30:53 +0700

    Made functions

--- main.py
+++ main.py
@@ -71,13 +71,56 @@ for month in range(12):
 
 
 if individual == local.p1 or individual == local.p2:
-    pass
+    if personalD1 < D < personalD2:
+        N = S1 * (D - personalD1)
+    elif personalD2 < D < personalD3:
+        N = S1 * (personalD2 - personalD1) + S2 * (D - personalD2)
+    elif personalD3 < D < personalD4:
+        N = S1 * (personalD2 - personalD1) + S2 * (personalD3 - personalD2) + S3 * (D - personalD3)
+    elif personalD4 < D < personalD5:
+        N = S1 * (personalD2 - personalD1) + S2 * (personalD3 - personalD2) + S3 * (personalD4 - personalD3) + S4 * (
+                D - personalD4)
+    elif personalD5 < D < personalD6:
+        N = S1 * (personalD2 - personalD1) + S2 * (personalD3 - personalD2) + S3 * (personalD4 - personalD3) + S4 * (
+                personalD5 - personalD4) + S5 * (D - personalD5)
+    else:
+        N = S1 * (personalD2 - personalD1) + S2 * (personalD3 - personalD2) + S3 * (personalD4 - personalD3) + S4 * (
+                personalD5 - personalD4) + S5 * (personalD6 - personalD5) + S6 * (D - personalD6)
 
 elif individual == local.f1 or individual == local.f2:
-    pass
+    if familyD1 < D < familyD2:
+        N = S1 * (D - familyD1)
+    elif familyD2 < D < familyD3:
+        N = S1 * (familyD2 - familyD1) + S2 * (D - familyD2)
+    elif familyD3 < D < familyD4:
+        N = S1 * (familyD2 - familyD1) + S2 * (familyD3 - familyD2) + S3 * (D - familyD3)
+    elif familyD4 < D < familyD5:
+        N = S1 * (familyD2 - familyD1) + S2 * (familyD3 - familyD2) + S3 * (familyD4 - familyD3) + S4 * (
+                D - familyD4)
+    elif familyD5 < D < familyD6:
+        N = S1 * (familyD2 - familyD1) + S2 * (familyD3 - familyD2) + S3 * (familyD4 - familyD3) + S4 * (
+                familyD5 - familyD4) + S5 * (D - familyD5)
+    else:
+        N = S1 * (familyD2 - familyD1) + S2 * (familyD3 - familyD2) + S3 * (familyD4 - familyD3) + S4 * (
+                familyD5 - familyD4) + S5 * (familyD6 - familyD5) + S6 * (D - familyD6)
 
 elif individual == local.sp1 or individual == local.sp2:
-    pass
+    if singleparentD1 < D < singleparentD2:
+        N = S1 * (D - singleparentD1)
+    elif singleparentD2 < D < singleparentD3:
+        N = S1 * (singleparentD2 - singleparentD1) + S2 * (D - singleparentD2)
+    elif singleparentD3 < D < singleparentD4:
+        N = S1 * (singleparentD2 - singleparentD1) + S2 * (singleparentD3 - singleparentD2) + S3 * (D - singleparentD3)
+    elif singleparentD4 < D < singleparentD5:
+        N = S1 * (singleparentD2 - singleparentD1) + S2 * (singleparentD3 - singleparentD2) + S3 * (
+                singleparentD4 - singleparentD3) + S4 * (D - singleparentD4)
+    elif singleparentD5 < D < singleparentD6:
+        N = S1 * (singleparentD2 - singleparentD1) + S2 * (singleparentD3 - singleparentD2) + S3 * (
+                singleparentD4 - singleparentD3) + S4 * (singleparentD5 - singleparentD4) + S5 * (D - singleparentD5)
+    else:
+        N = S1 * (singleparentD2 - singleparentD1) + S2 * (singleparentD3 - singleparentD2) + S3 * (
+                singleparentD4 - singleparentD3) + S4 * (singleparentD5 - singleparentD4) + S5 * (
+                singleparentD6 - singleparentD5) + S6 * (D - singleparentD6)
 
 else:
     print(Tax_Error)
@@ -85,7 +128,5 @@ else:
 
 
 
-
-
 # TODO: вывод, по вопросу задачи
-
+print(D)
